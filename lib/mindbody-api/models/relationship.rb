2bca932c9d51e9fc790a2c6bcc890f02e2c7a144
BaronWolfenstein <selimthegrim@gmail.com>
2013-08-31 12:20:32 -0700

    Create relationship.rb
    adding relationship support

---
+++ lib/mindbody-api/models/relationship.rb
@@ -0,0 +1,10 @@
+module MindBody
+  module Models
+    class Relationship < Base
+      attribute :id, Integer
+      attribute :relationship_name1, String
+      attribute :relationship_name2, String
+    end
+  end
+end
+    
