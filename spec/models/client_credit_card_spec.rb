11959accc472e86faa1b7176ec046d170c5c25b0
BaronWolfenstein <selimthegrim@gmail.com>
2013-08-31 17:53:29 -0700

    Update client_credit_card_spec.rb

--- spec/models/client_credit_card_spec.rb
+++ spec/models/client_credit_card_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe MindBody::Models::ClientCreditCard do
-  it {should respond_to(card_number)}
+  it {should respond_to(:card_number)}
   it {should respond_to(:card_holder)}
   it {should respond_to(:city)}
   it {should respond_to(:address)}
