fedaa4f53042a2933de2d8f358b44d7f05c506f3
Stafford Brunk <wingrunr21@gmail.com>
2013-10-27 10:37:57 -0600

    Add more attributes to Staff model

--- lib/mindbody-api/models/staff.rb
+++ lib/mindbody-api/models/staff.rb
@@ -13,9 +13,17 @@ module MindBody
       attribute :country, String
       attribute :postal_code, String
       attribute :foreign_zip, String
+      attribute :login_locations, Array[Location]
       attribute :name, String
       attribute :first_name, String
       attribute :last_name, String
+      attribute :image_url, String
+      attribute :bio, String
+      attribute :is_male, Boolean
+      attribute :username, String # API does not currently return this
+
+      # For API compatibility between Client
+      alias photo_url image_url
     end
   end
 end
--- spec/models/staff_spec.rb
+++ spec/models/staff_spec.rb
@@ -13,7 +13,17 @@ describe MindBody::Models::Staff do
   it { should respond_to(:country) }
   it { should respond_to(:postal_code) }
   it { should respond_to(:foreign_zip) }
+  it { should respond_to(:login_locations) }
   it { should respond_to(:name) }
   it { should respond_to(:first_name) }
   it { should respond_to(:last_name) }
+  it { should respond_to(:image_url) }
+  it { should respond_to(:bio) }
+  it { should respond_to(:is_male) }
+  it { should respond_to(:username) }
+
+  it 'should alias photo_url to image_url' do
+    subject.image_url = 'http://foobar.com/image.jpg'
+    expect(subject.photo_url).to eql('http://foobar.com/image.jpg')
+  end
 end
