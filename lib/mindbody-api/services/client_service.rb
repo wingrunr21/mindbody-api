fdff6979ad986efffe0e05c00c6d3f98be57225b
Stafford Brunk <wingrunr21@gmail.com>
2014-01-22 12:52:55 -0700

    Make sure user_first_name and user_last_name are required for send_user_new_password

--- lib/mindbody-api/services/client_service.rb
+++ lib/mindbody-api/services/client_service.rb
@@ -3,16 +3,16 @@ module MindBody
     class ClientService < Service
       service 'ClientService'
 
-      operation :add_arrival,                    :required => [:client_id, :location_id],         :locals => false
+      operation :add_arrival,                    :required => [:client_id, :location_id],                                   :locals => false
       operation :add_or_update_clients,          :required => [:clients]
       operation :get_clients,                    :required => [:client_ids]
-      operation :get_custom_client_fields,                                                        :locals => false
-      operation :get_client_indexes,                                                              :locals => false
+      operation :get_custom_client_fields,                                                                                  :locals => false
+      operation :get_client_indexes,                                                                                        :locals => false
       operation :get_client_contact_logs,        :required => [:client_id]
       operation :add_or_update_contact_logs,     :required => [:contact_logs]
-      operation :get_contact_log_types,                                                           :locals => false
+      operation :get_contact_log_types,                                                                                     :locals => false
       operation :upload_client_document,         :required => [:client_id, :filename, :bytes]
-      operation :get_client_referral_types,                                                       :locals => false
+      operation :get_client_referral_types,                                                                                 :locals => false
       operation :get_active_client_memberships,  :required => [:client_id]
       operation :get_client_contracts,           :required => [:client_id]
       operation :get_client_account_balances,    :required => [:client_ids]
@@ -20,10 +20,10 @@ module MindBody
       operation :get_client_visits,              :required => [:client_id]
       operation :get_client_purchases,           :required => [:client_id]
       operation :get_client_schedule,            :required => [:client_id]
-      operation :get_required_client_fields,                                                      :locals => false
-      operation :validate_login,                 :required => [:username, :password],             :locals => false
+      operation :get_required_client_fields,                                                                                :locals => false
+      operation :validate_login,                 :required => [:username, :password],                                       :locals => false
       operation :update_client_services,         :required => [:client_services]
-      operation :send_user_new_password,         :required => [:email],                           :locals => false
+      operation :send_user_new_password,         :required => [:user_email, :user_first_name, :user_last_name],             :locals => false
     end
   end
 end
