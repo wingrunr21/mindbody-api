0125fa98cf5f30201d37af0290a345bd47520e56
Stafford Brunk <wingrunr21@gmail.com>
2014-03-21 09:33:36 -0600

    Add get_staff_img_url to StaffService

--- lib/mindbody-api/services/staff_service.rb
+++ lib/mindbody-api/services/staff_service.rb
@@ -6,6 +6,7 @@ module MindBody
       operation :get_staff
       operation :get_staff_permissions
       operation :add_or_update_staff,     :required => [:staff]
+      operation :get_staff_img_url,       :required => [:staffid]
     end
   end
 end
--- spec/services/staff_service_spec.rb
+++ spec/services/staff_service_spec.rb
@@ -6,4 +6,5 @@ describe MindBody::Services::StaffService do
   it { should respond_to(:get_staff) }
   it { should respond_to(:get_staff_permissions) }
   it { should respond_to(:add_or_update_staff) }
+  it { should respond_to(:get_staff_img_url) }
 end
