d729821ba1ab40e4e479192c4997d182bd169ba6
David Macht <dmacht@email.com>
2015-08-13 11:11:19 -0400

    Add and remove clients from classes

--- lib/mindbody-api/services/class_service.rb
+++ lib/mindbody-api/services/class_service.rb
@@ -4,9 +4,11 @@ module MindBody
       service "ClassService"
 
       operation :get_classes
-      operation :get_class_visits,           required:[:class_id]
+      operation :get_class_visits,             required:[:class_id]
       operation :get_class_descriptions
       operation :get_class_schedules
+      operation :add_clients_to_classes,       required: [:class_ids, :client_ids]
+      operation :remove_clients_from_classes,  required: [:class_ids, :client_ids]
     end
   end
 end
